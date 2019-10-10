52f32edf4a67837a3c25060be33ac6b7c31c58d2
hr4official <hardikrthod3695@gmail.com>
2019-10-10 10:09:52 +0530

    update

--- mbit/mbit/urls.py
+++ mbit/mbit/urls.py
@@ -1,6 +1,6 @@
 """mbit URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more  information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from appone.views import homepage
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('',include('appone.urls')),
     path('admin/', admin.site.urls),
 ]
